f012fa6851204c838e8a33996dabe66fd1ba6d50
rkorzen <korzeniewski@gmail.com>
2025-03-22 15:28:54 +0100

    update settings.py

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', "146.190.142.165" ]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") or ['localhost', '127.0.0.1' ]
 
 # Application definition
 
